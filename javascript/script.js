8eb5161f843444dda00806fc77edd6a58b97561f
zelula1955 <zelula1955@gmail.com>
2021-07-09 17:46:25 -0300

    Projeto de reconstrução do jogo da Cobrinha no BootCamp (Projeto/Desafio)

---
+++ javascript/script.js
@@ -0,0 +1,108 @@
+let canvas = document.getElementById("snake"); //criar elemento que irá rodar o jogo
+let context = canvas.getContext("2d"); //....
+let box = 32;
+let nivel = 1;
+let veloz = 300;
+let splitsnake = 5;
+
+let snake = []; //criar cobrinha como lista, já que ela vai ser uma série de coordenadas, que quando pintadas, criam os quadradinhos
+snake[0] ={
+    x: 8 * box,
+    y: 8 * box
+}
+let direction = "left";
+let food ={
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
+function criarBG(){
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16*box, 16*box); //desenha o retângulo usando x e y e a largura e altura setadas
+}
+
+function criarCobrinha (){
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+function drawFood (){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+//quando um evento acontece, detecta e chama uma função
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
+function iniciarJogo(){  
+    if(snake[0].x > 16*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16*box;
+    if(snake[0].y > 16*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16*box;
+    
+    for(i = 1; i < snake.length; i++)
+    {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y)
+        {
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+        else
+        {
+            console.log ("limite: " + snake.length + " Velocidade: " + veloz);
+            if (snake.length >= splitsnake && nivel < 4) 
+            {     
+                veloz = (veloz -100);
+                setInterval(iniciarJogo, veloz);
+                splitsnake += 5
+                alert(splitsnake + "Continuar NIVEL :" + (nivel += 1) );
+            }
+            else
+            {  
+                if (nivel > 3)
+                {              
+                    clearInterval(jogo);
+                    alert('WINNER: :)');
+                }
+            }
+        }
+    }
+
+    criarBG();
+    criarCobrinha();
+    drawFood();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop(); //pop tira o último elemento da lista
+    }else{
+        food.x = Math.floor(Math.random() * 15 +1) * box;
+        food.y = Math.floor(Math.random() * 15 +1) * box;
+    }
+    
+    let newHead ={
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead); //método unshift adiciona como primeiro quadradinho da cobrinha
+
+}
+
+let jogo = setInterval(iniciarJogo, veloz);
